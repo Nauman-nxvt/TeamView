02a16bf5c68da4599ea04e1939faf64bb0eb2359
Nauman Tahir <nomibzu@gmail.com>
2018-10-26 02:54:05 +0200

    Added local storage and Higher Order Component for hover

--- src/components/Member.jsx
+++ src/components/Member.jsx
@@ -1,44 +1,36 @@
-import React, {Component} from 'react';
-import {RoundBtn} from "./RoundBtn";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import {RoundImg} from "./RoundImg";
-import {AvatarText} from "./AvatarText";
+import React, {Component, Fragment} from 'react';
+import {RoundBtn} from "./sub-components/RoundBtn";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {RoundImg} from "./sub-components/RoundImg";
+import {AvatarText} from "./sub-components/AvatarText";
+import Hoverable from './Hoverable'
 
-export default class Member extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            memberHovered: false,
-        }
-    }
-
-    onMouseEnter = () => {
-        this.setState({memberHovered: true});
-    };
-
-    onMouseLeave = () => {
-        this.setState({memberHovered: false});
-    };
-
+class Member extends Component {
     render() {
-        const {member} = this.props;
-        return(
-            <div className={'team-block'} style={this.state.memberHovered ? {backgroundColor: '#E2F4EA'} : {}} onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        const {member, hovered} = this.props;
+        return (
+            <Fragment>
                 {
-                    this.state.memberHovered ?
+                    hovered ?
                         <RoundBtn
                             style={styles.removeBtn}
-                            onClick={()=>{this.props.removeMember(member.id)}}
+                            onClick={() => {
+                                this.props.removeMember(member.id)
+                            }}
                             content={<FontAwesomeIcon icon={faTimes}/>}
                         /> :
                         <RoundImg style={styles.avatarImg} image={member.picture}/>
                 }
                 <AvatarText role={member.role} username={member.username}/>
-            </div>
+            </Fragment>
         )
     }
 }
+
+const MemberHoverable = Hoverable(Member, 'team-block');
+
+export default MemberHoverable;
 
 const styles = {
     removeBtn: {
--- src/components/sub-components/RoundImg.jsx
+++ src/components/sub-components/RoundImg.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const RoundImg = props => {
     return(
         <div id={'avatar-img'}>
-            <img style={props.style} src={require(`../assets/images/${props.image}`)} alt={'avatar'}/>
+            <img style={props.style} src={require(`../../assets/images/${props.image}`)} alt={'avatar'}/>
         </div>
     )
 };
